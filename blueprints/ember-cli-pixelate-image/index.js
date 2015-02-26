453a12ca29d7f94df62fb1b0cd41da73e6cde3ab
Charles Lowell <cowboyd@frontside.io>
2015-02-25 22:14:18 -0600

    fix blueprint generator

--- blueprints/ember-cli-pixelate-image/index.js
+++ blueprints/ember-cli-pixelate-image/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   description: 'default blueprint for ember-cli-pixelate-image',
 
+  normalizeEntityName: function() {
+    //no required entity name
+  },
+
   afterInstall: function(options) {
     return this.addBowerPackageToProject('pixelate');
   }
