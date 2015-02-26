3cfecc61913a7e8e2e538a24cf51d085637c550b
Charles Lowell <cowboyd@frontside.io>
2015-02-25 23:23:03 -0600

    automatically add 'data:' to the 'img-src' CSP

--- index.js
+++ index.js
@@ -4,6 +4,19 @@
 module.exports = {
   name: 'ember-cli-pixelate-image',
 
+  config: function(environment, appConfig) {
+    //add data to content security policy
+    var imgSrcPolicy = appConfig.contentSecurityPolicy['img-src'];
+    if (!/data:/.test(imgSrcPolicy)) {
+      imgSrcPolicy = imgSrcPolicy + " data:";
+    }
+    return {
+      contentSecurityPolicy: {
+        'img-src': imgSrcPolicy
+      }
+    };
+  },
+
   included: function(app) {
     app.import(app.bowerDirectory + '/pixelate/pixelate.js');
     app.import('vendor/pixelate-image.css');
